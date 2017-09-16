99814587ed388ecc3d25b122cd99cc42f7459019
BelialDaniel <belialdaniel@gmail.com>
2017-09-16 14:35:33 -0500

    Initial commit

---
+++ app/src/main/java/com/dokkastudios/simpleindicatortoviewpager/MainApp.java
@@ -0,0 +1,15 @@
+package com.dokkastudios.simpleindicatortoviewpager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainApp extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_main_app);
+    }
+}
